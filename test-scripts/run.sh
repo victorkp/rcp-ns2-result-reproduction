7dfebf43b91a4406981fd7a8f9370e77f0ef0a87
vkpendergrast <v.kaiser.pendergrast@gmail.com>
2016-05-28 19:45:22 -0700

    Gitignore update, minor fix

--- test-scripts/run.sh
+++ test-scripts/run.sh
@@ -15,11 +15,11 @@ load=0.9
 shape=1.2
 mean=25
 
-rcp_in=traces/rcp-flow.tr
+rcp_in=../test-output/rcp-flow.tr
 rcp_out=../test-output/rcp-flow-vs-delay.out
 rcp_log=../test-output/rcp.log
 
-tcp_in=traces/tcp-flow.tr
+tcp_in=../test-output/tcp-flow.tr
 tcp_out=../test-output/tcp-flow-vs-delay.out
 tcp_log=../test-output/tcp.log
 
